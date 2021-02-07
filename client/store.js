b6fb9cc6113a637060c43a531aa8d8a33e99be71
Mert Dogar <mertdogar@gmail.com>
2021-02-07 17:35:39 +0300

    :sparkles: Add stun/turn servers for peerjs

--- client/store.js
+++ client/store.js
@@ -94,7 +94,32 @@ const connect = ({host, port, roomId}) => {
     console.log('User disconnected', payload);
   });
 
-  myPeer = new Peer();
+  myPeer = new Peer({
+    config: {
+      iceServers: [
+        {url:'stun:stun01.sipphone.com'},
+        {url:'stun:stun.ekiga.net'},
+        {url:'stun:stun.fwdnet.net'},
+        {url:'stun:stun.ideasip.com'},
+        {url:'stun:stun.iptel.org'},
+        {url:'stun:stun.rixtelecom.se'},
+        {url:'stun:stun.schlund.de'},
+        {url:'stun:stun.l.google.com:19302'},
+        {url:'stun:stun1.l.google.com:19302'},
+        {url:'stun:stun2.l.google.com:19302'},
+        {url:'stun:stun3.l.google.com:19302'},
+        {url:'stun:stun4.l.google.com:19302'},
+        {url:'stun:stunserver.org'},
+        {url:'stun:stun.softjoys.com'},
+        {url:'stun:stun.voiparound.com'},
+        {url:'stun:stun.voipbuster.com'},
+        {url:'stun:stun.voipstunt.com'},
+        {url:'stun:stun.voxgratia.org'},
+        {url:'stun:stun.xten.com'},
+        {'url': 'turn:numb.viagenie.ca', 'credential': '7kSQDryCsV3M3VLq', 'username': 'mertdogar@gmail.com'},
+      ]
+    }
+  });
   myPeer.on('open', id => {
     stores.update('main', {me: {id, x: 0, y: 0, z: 0, micEnabled: true, videoEnabled: true}});
     socket.emit('join-room', roomId, store.me);
