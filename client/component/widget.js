91c481a523b96cd52f6064dafa241647421be0aa
Mert Dogar <mertdogar@gmail.com>
2021-02-09 08:42:51 +0300

    :art: Widget: Improve style

--- client/component/widget.js
+++ client/component/widget.js
@@ -11,6 +11,9 @@ import {Action} from '../store';
 const useStyles = createUseStyles({
   root: {
     display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
     position: 'absolute',
     top: 0,
     left: 0,
@@ -21,6 +24,16 @@ const useStyles = createUseStyles({
     backgroundSize: `100%`,
     backgroundRepeat: 'no-repeat'
   },
+  handle: {
+    color: 'white',
+    backgroundColor: '#ff8a65',
+    padding: 4,
+    textAlign: 'center',
+    fontSize: '0.8em',
+  },
+  body: {
+    flex: 1
+  }
 })
 
 
@@ -29,7 +42,6 @@ export default function({id, ...props}) {
   const [store, setStore] = useStore('main');
 
   const widget = store.widgets.find(item => item.id == id);
-
 
   const onDragEnd = useCallback((a, b) => {
     console.log('end');
@@ -40,20 +52,22 @@ export default function({id, ...props}) {
     })
   });
 
-
   const volume = utils.getVolumeByPositions(widget, store.me);
 
   return (
     <Draggable position={{x: widget.x, y: widget.y, z: widget.z}} onStop={onDragEnd} scale={1}>
-      <div className={classnames(classes.root)} style={{width: widget.width, height: widget.height}}>
-        {
-          widget.type === 'player' &&
-          <ReactPlayer volume={volume} playing={true} loop={true} playsinline={true} width={widget.width} height={widget.height} url={widget.data} />
-        }
-        {
-          widget.type === 'iframe' &&
-          <iframe src={widget.data} />
-        }
+      <div className={classnames(classes.root)}>
+        <div className={classes.handle}>{widget.name}</div>
+        <div className={classes.body} style={{width: widget.width, height: widget.height}}>
+          {
+            widget.type === 'player' &&
+            <ReactPlayer volume={volume} playing={true} loop={true} playsinline={true} width={widget.width} height={widget.height} url={widget.data} />
+          }
+          {
+            widget.type === 'iframe' &&
+            <iframe frameborder="0" src={widget.data} />
+          }
+        </div>
       </div>
     </Draggable>
   );
