a204f5b9b3d1d09173af4a900942b62bbfac249a
Mert Dogar <mertdogar@gmail.com>
2021-02-10 11:34:21 +0300

    :art: Users: Move users to left bottom

--- client/component/users.js
+++ client/component/users.js
@@ -7,7 +7,7 @@ import {useStore} from 'yaver';
 const useStyles = createUseStyles({
   root: {
     position: 'fixed',
-    top: 0,
+    bottom: 0,
     left: 0,
     display: 'flex',
     flexDirection: 'column',
